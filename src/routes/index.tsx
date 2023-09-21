8c500edf9620a729e944be0c8204f7a01c8be123
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2023-09-21 16:48:04 -0300

    configurando as rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {}
+import {Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+
+export function AppRoute(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/app" element={<h1>Página inicial</h1>}/>
+                <Route path="*" element={<Navigate to={"/app"}/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
